83e01a0d348049130f0153ff3627f44ac5bd85a0
123-deep <152241662+123-deep@users.noreply.github.com>
2023-12-18 21:32:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+
+<html>
+
+<head>
+    
+</head>
+
+    <body>
+   
+        <a href="" data-toggle="modal" data-target="#myModal">clicl me </a>
+        
+        
+         <?php require 'login.php'?>
+    
+    </body>
+</html>
